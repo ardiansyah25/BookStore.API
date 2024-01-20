6974d850c046a99dd6ddc46a6d7017c9663a653b
Ardiansyah <aardi25@GMAIL.COM>
2024-01-20 16:47:32 +0700

    finish

--- BookStore.API/Repository/IAccountRepository.cs
+++ BookStore.API/Repository/IAccountRepository.cs
@@ -10,5 +10,6 @@ namespace BookStore.API.Repository
     public interface IAccountRepository
     {
         Task<IdentityResult> SignUpAsync(SignUpModel signUpModel);
+        Task<string> LoginAsync(SignInModel signInModel);
     }
 }
